ab676af868cd8b9097bf50d615cdbaadaa4e6c7e
znobakht <nobakht9010@yahoo.com>
2024-04-20 14:16:32 +0330

    bcrypt installed and used for hashing passwords

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { registrationSchema } from "../../validation/validationSchemas.mjs";
 import User from "../../models/User.mjs";
 
@@ -7,7 +8,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.send("Hello World");
 });
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res) => {
   const { error, value } = registrationSchema.validate(req.body);
   if (error) {
     // Return validation errors
@@ -18,28 +19,35 @@ router.post("/", async (req, res)=>{
 
   try {
     // Check if the username or email is already taken
-    const existingUser = await User.findOne({ $or: [{ username: value.username }, { email: value.email }] });
+    const existingUser = await User.findOne({
+      $or: [{ username: value.username }, { email: value.email }],
+    });
     if (existingUser) {
-      return res.status(400).json({ error: 'Username or email is already taken' });
+      return res
+        .status(400)
+        .json({ error: "Username or email is already taken" });
     }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(value.password, 10);
 
     // Create a new user
     const newUser = new User({
       name: value.name,
       username: value.username,
       email: value.email,
-      password: value.password, // Note: In practice, you would hash the password before storing it
+      password: hashedPassword, // Note: In practice, you would hash the password before storing it
     });
 
     // Save the new user to the database
     await newUser.save();
 
     // Respond with success message
-    res.json({ message: 'User registered successfully', user: newUser });
+    res.json({ message: "User registered successfully", user: newUser });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
-} )
+});
 
 export default router;
