e148bc42540c03a5ce23bdc2196c3a7e1b01185c
znobakht <nobakht9010@yahoo.com>
2024-04-20 14:08:59 +0330

    cors, urlencoded, and json parser used to have access in the req.body

--- app.mjs
+++ app.mjs
@@ -1,15 +1,24 @@
 // app.mjs
 import express from "express";
+const app = express();
+
 import dotenv from "dotenv";
 dotenv.config();
+
 import mongoose from "mongoose";
+
+import cors from "cors";
+app.use(cors());
 
 import userRouter from "./routes/api/users.mjs";
 
-const app = express();
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Use the routers
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
