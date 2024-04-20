fa008d7fe6afe8646b7ba706621ac976987f5952
znobakht <nobakht9010@yahoo.com>
2024-04-20 14:35:42 +0330

    jwt installed and used to generate token for the user

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import { registrationSchema } from "../../validation/validationSchemas.mjs";
+import jwt from "jsonwebtoken";
+import { loginSchema, registrationSchema } from "../../validation/validationSchemas.mjs";
 import User from "../../models/User.mjs";
 
 const router = express.Router();
@@ -41,9 +42,49 @@ router.post("/", async (req, res) => {
 
     // Save the new user to the database
     await newUser.save();
+    // Omit password from newUser
+    const { password, __v, ...userWithoutPassword } = newUser.toObject();
 
     // Respond with success message
-    res.json({ message: "User registered successfully", user: newUser });
+    res.json({
+      message: "User registered successfully",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+router.post("/login", async (req, res) => {
+  const { error, value } = loginSchema.validate(req.body);
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: error.details.map((detail) => detail.message) });
+  }
+
+  try {
+    // Check if the user exists
+    const user = await User.findOne({ email: value.email });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await bcrypt.compare(value.password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    // Password is correct, generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" } // Token expires in 1 hour
+    );
+
+    // Respond with success message and JWT token
+    res.json({ message: "Login successful", token });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
