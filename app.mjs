2ae285042f4d7f0b97cdd8aa571e9c53b6ae858b
znobakht <nobakht9010@yahoo.com>
2024-04-20 12:06:05 +0330

    mongoose used to connect to the database

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import mongoose from "mongoose";
 
 import userRouter from "./routes/api/users.mjs";
 
@@ -15,3 +16,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+try {
+const dbAddress = process.env.mongoURL || "mongodb://127.0.0.1:27017"
+  await mongoose.connect(`${dbAddress}/SayaDatabase`);
+
+  console.log("Connected to MongoDB");
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+}
