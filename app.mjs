e8cceaf15d2dc768dd67e9e4fd30ae93570719e9
znobakht <nobakht9010@yahoo.com>
2024-04-21 07:04:57 +0330

    exporting app an sever to be used in test

--- app.mjs
+++ app.mjs
@@ -22,7 +22,7 @@ app.use("/users", userRouter);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 (async () => {
@@ -35,3 +35,8 @@ app.listen(PORT, () => {
     console.error("Error connecting to MongoDB:", error);
   }
 })();
+
+export default app
+
+export {server}
+
