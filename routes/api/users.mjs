b3ccee6deb6c21e9aa0de76b3cd04d87ce185abd
znobakht <nobakht9010@yahoo.com>
2024-04-25 21:36:36 +0330

    imported folder name edited

--- routes/api/users.mjs
+++ routes/api/users.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { editUserSchema, loginSchema, registrationSchema } from "../../userValidation/validationSchemas.mjs";
+import { editUserSchema, loginSchema, registrationSchema } from "../../validation/userValidationSchemas.mjs";
 import User from "../../models/User.mjs";
 import authMiddleware from "../../middleware/authMiddleware.mjs";
 import xss from "xss";
